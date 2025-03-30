f5f7e3139aff37f2103ab17160df689b9061e0a6
lindavik <linda.viksne@gmail.com>
2025-03-30 22:08:40 +0200

    add basic reader

---
+++ tests/reader_test.py
@@ -0,0 +1,72 @@
+import os
+import pytest
+from vetnote.reader import FileReader
+
+
+class TestFileReader:
+
+    @pytest.fixture
+    def file_reader(self) -> FileReader:
+        return FileReader()
+    
+    @pytest.fixture
+    def expected_file_content(self):
+        return (
+            '{\n'
+            '  "patient": {\n'
+            '    "name": "Darth Vader",\n'
+            '    "species": "Dog (Canine - Domestic)",\n'
+            '    "breed": "Chiuaua",\n'
+            '    "gender": "male",\n'
+            '    "neutered": false,\n'
+            '    "date_of_birth": "2019-01-01",\n'
+            '    "microchip": null,\n'
+            '    "weight": "3.2 kg"\n'
+            '  },\n'
+            '  "consultation": {\n'
+            '    "date": "2025-02-21",\n'
+            '    "time": "12:37",\n'
+            '    "reason": "Repeat Consultation",\n'
+            '    "type": "Outpatient",\n'
+            '    "clinical_notes": [\n'
+            '      {\n'
+            '        "type": "general",\n'
+            '        "note": "Doing a lot better, '
+            'cough stopped\\nstarted running and '
+            'playing\\neats food without issues\\nseems better and more lively overall"\n'
+            '      }\n'
+            '    ],\n'
+            '    "treatment_items": {\n'
+            '      "procedures": [\n'
+            '        {\n'
+            '          "date": "2025-02-21",\n'
+            '          "time": "12:45",\n'
+            '          "name": "Consultation - Repeat",\n'
+            '          "code": "con010",\n'
+            '          "quantity": 1,\n'
+            '          "total_price": 6450,\n'
+            '          "currency": "GBP"\n'
+            '        }\n'
+            '      ],\n'
+            '      "medicines": [],\n'
+            '      "prescriptions": [],\n'
+            '      "foods": [],\n'
+            '      "supplies": []\n'
+            '    },\n'
+            '    "diagnostics": []\n'
+            '  }\n'
+            '}\n'
+        )
+
+    def test_read_file_valid(self, file_reader, expected_file_content):
+        file_path = os.path.join(os.path.dirname(__file__), "data", "consultation.json")
+        
+        actual_file_content = file_reader.read_file(file_path)
+
+        assert actual_file_content == expected_file_content
+
+    def test_read_file_not_found(self, file_reader):
+        file_path = os.path.join(os.path.dirname(__file__), "data", "non_existent.json")
+
+        with pytest.raises(FileNotFoundError, match=f"File '{file_path}' not found."):
+            file_reader.read_file(file_path)
--- vetnote/main.py
+++ vetnote/main.py
@@ -1,15 +1,29 @@
-def main():
-    print("Welcome to VetNote")
+import sys
 
-    # Todo steps:
-    # 1. read the input/data files
+from vetnote.reader import FileReader
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <input_file_path>")
+        print("Please provide a file path")
+        sys.exit(1)
+    
+    file_path = sys.argv[1]
+    file_reader = FileReader()
+
+    try:
+      file_content = file_reader.read_file(file_path)
+      print(f"Contents of the file:\n{file_content}")
+    except FileNotFoundError as e:
+        print(f"An exception occured while reading the input files: {e}")    
+
     # 2. process the data ie translate it into an LLM prompt
     # 3. call the LLM
     # 4. process the LLM output, including error handling
     # 5. include fallback, if first LLM call fails,
     # depending on error retry and then pivot to second LLM
     # 6. write the output to a file in the solution folder
-    # Fun stretch goal: add streamlit UI
 
 
 if __name__ == "__main__":
